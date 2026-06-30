6d78fd52e580b5185a864e993d92d869c0107ba8
Paola Ghione <ghionep@gmail.com>
2026-06-30 18:04:11 +0200

    fix for format compliance and fix FastMCP import for mcp dev compatibility

--- mcp/prompt/server.py
+++ mcp/prompt/server.py
@@ -1,16 +1,19 @@
-from fastmcp import FastMCP
+# from fastmcp import FastMCP
+from mcp.server.fastmcp import FastMCP
 from mcp.server.fastmcp.prompts import base
 
 mcp = FastMCP()
 
+
 @mcp.prompt(title="Attention Prompt", description="This prompt is used to draw attention to a specific message.")
-async def show_prompt(message:str) ->str:
+async def show_prompt(message: str) -> str:
     return f"Please pay attention to {message}"
 
 
-@mcp.prompt(title="Debug Code", description="This prompt is used to debug code snippets.")
+@mcp.prompt(title="Debug Code",
+            description="This prompt is used to debug code snippets.")
 async def debug_code(code: str) -> list[base.Message]:
-   return [
-       base.UserMessage("I can see this code has errors."),
-       base.AssistantMessage("What have you tried so far?")
-   ]
+    return [
+        base.UserMessage("I can see this code has errors."),
+        base.AssistantMessage("What have you tried so far?")
+    ]
