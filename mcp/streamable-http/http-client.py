b1d32411c3c8219c9902d4d46eb5db3d36a8b37a
Paola Ghione <ghionep@gmail.com>
2026-07-02 17:26:52 +0200

    fix format compliance in mcp/streamable-http/http-client.py and server-with-redis.py

--- mcp/streamable-http/http-client.py
+++ mcp/streamable-http/http-client.py
@@ -5,24 +5,28 @@ from mcp.client.streamable_http import streamable_http_client
 
 # from mcp.client.stdio import stdio_client
 
+
 async def main():
     async with streamable_http_client("http://localhost:8000/mcp") as (
         read_stream,
         write_stream,
-        _, 
+        _,
     ):
         async with ClientSession(read_stream, write_stream) as session:
             await session.initialize()
             tools = await session.list_tools()
             # await session.list_resources
             print(f"Available tools:{[tool.name for tool in tools.tools]}")
-            
-            greeting_tool = next((tool for tool in tools.tools if tool.name == "Greetings"), None)
+
+            greeting_tool = next((tool for tool in tools.tools
+                                  if tool.name == "Greetings"),
+                                 None)
             if greeting_tool:
-                tool_result = await session.call_tool(name="Greetings",
-                                  arguments={"name":"Aref"})
-                greeting = tool_result.structuredContent # other result types are EmbeddedResource and ImageContent
+                tool_result = await session.call_tool(
+                    name="Greetings", arguments={"name": "Aref"})
+                greeting = tool_result.structuredContent  # other result types
+                # are EmbeddedResource and ImageContent
                 print(greeting.get("message"))
 
-if __name__=="__main__":
+if __name__ == "__main__":
     asyncio.run(main())
--- mcp/streamable-http/server-with-redis.py
+++ mcp/streamable-http/server-with-redis.py
@@ -4,29 +4,37 @@ import asyncio
 import redis
 
 # mcp = FastMCP(stateless_http=True)
+# # if you want you can run stateless http server,
+# but then you will not be able to use redis to store session data
 mcp = FastMCP()
+
 
 class Greeting(BaseModel):
     message: str
 
+
 @mcp.tool("Greetings")
-def greetings (name:str , ctx: Context) -> Greeting:
-    """A tool function that accepts a parameter called name and returns a personalised greeting message."""
+def greetings(name: str, ctx: Context) -> Greeting:
+    """A tool function that accepts a parameter called name
+    and returns a personalised greeting message."""
 
     session_id = ctx.session_id
 
-    redis_client = redis.Redis(host='localhost', port=6379, username="", password="")
+    redis_client = redis.Redis(host='localhost',
+                               port=6379,
+                               username="", password="")
 
     stored_name = redis_client.get(session_id)
 
-    if stored_name == None:
+    if stored_name is None:
         redis_client.set(session_id, name)
     else:
         name = stored_name.decode('utf-8')
 
-    ctx.info("Returning results..") # Use Context to generate Logs
+    ctx.info("Returning results..")  # Use Context to generate Logs
 
-    return  Greeting(message= f"Hello, {name}")
+    return Greeting(message=f"Hello, {name}")
+
 
 async def main():
     await mcp.run_streamable_http_async()
